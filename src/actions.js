baef6d17d3b9076c9379b10e806d2dea276fd680
Opium Pro <ilove@opium.pro>
2021-08-02 02:43:50 +0200

    upd version

--- src/actions.js
+++ src/actions.js
@@ -1,7 +1,7 @@
 import { useStorage } from 'master-hook'
 
 export const makePunName = () => (dispatch, getState) => {
-  const { setPunName, firstName, lastName } = useStorage('names', dispatch, getState)
+  const { setPunName, firstName, lastName } = useStorage('names')
   const punName = 'Shm' + firstName.slice(1) + '-Shm' + lastName.slice(1)
   setPunName(punName)
 }
