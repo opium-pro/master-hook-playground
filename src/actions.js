b7e31b1d9d319bc0f52b8455c9cb2d5e49cea867
Denis Elianovsky <denis@opium.pro>
2021-09-24 17:50:52 +0200

    upd version

--- src/actions.js
+++ src/actions.js
@@ -3,14 +3,14 @@ import { random } from './utils/random'
 import { adverbs, adjectives, nouns } from './utils/words'
 
 
-export const makePunName = createAction(async () => {
+export const makePunName = createAction(() => {
   const { setPunName } = useStorage('names')
   const punName = `${adverbs[random(0, adverbs.length-1)]} ${adjectives[random(0, adjectives.length-1)]} ${nouns[random(0, nouns.length-1)]}`
   setPunName(punName)
 })
 
 
-export const makePunNameWithDelay = createAction(() => {
+export const makePunNameWithDelay = createAction(async () => {
   return new Promise((res) => {
     setTimeout(() => {
       makePunName()
